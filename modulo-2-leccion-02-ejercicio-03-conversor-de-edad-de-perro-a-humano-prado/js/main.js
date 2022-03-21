507afeb90435d4b89a5e9daeaf8c6341553beb23
PradoCarretero <p.carreteroayala@gmail.com>
2022-03-21 19:06:23 +0100

    add

---
+++ modulo-2-leccion-02-ejercicio-03-conversor-de-edad-de-perro-a-humano-prado/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const human = 8;
+let result= "";
+
+if (human === 1) { result = 15} else if (human === 2)  { console.log(result= 15+9)} else if (human >= 3)  { console.log(result= human *5)};
+
+
