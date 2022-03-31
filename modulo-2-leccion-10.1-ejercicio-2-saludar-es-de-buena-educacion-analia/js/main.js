0c443889fce69802aa3101c5dd7a79aa9133d797
Analía Río <analia.rio@gmail.com>
2022-03-31 20:00:21 +0200

    JS Greeting Is Good Manners Exercise

---
+++ modulo-2-leccion-10.1-ejercicio-2-saludar-es-de-buena-educacion-analia/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Estamos creando una aplicación web, y lo primero que queremos hacer es saludar al usuario por su nombre, ¡como es debido! Tenemos un array con el listado de usuarios de nuestra aplicación const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada']; y queremos conseguir otro array con los saludos, por ejemplo, 'Bienvenida Yolanda'. ¿Podríamos usar map para que nos echase una mano?
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const greeting = names.map((name) => `¡Bienvenida, ${name}! 😃`);
+console.log(greeting);
